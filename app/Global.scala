f8ccd4e2a41d80c9e4b303515ccc08ac106df909
unknown <esterlinkof@gmail.com>
2015-05-31 00:19:51 +0430

    music play html added

---
+++ app/Global.scala
@@ -0,0 +1,10 @@
+import play.api._
+import java.net.URL
+import javax.sound.sampled._
+/**
+ * Created by sina on 5/30/2015.
+ */
+object Global extends GlobalSettings {
+  override def  onStart(app:Application){
+  }
+}
